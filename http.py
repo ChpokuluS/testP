7bc087574c480d2040910ba34d000d9d51b7b1b5
ChpokuluS <86004400+ChpokuluS@users.noreply.github.com>
2021-06-16 17:22:24 +0300

    Delete http.py

--- http.py
+++
@@ -1,6 +0,0 @@
-import BaseHTTPServer, SimpleHTTPServer
-import ssl
-
-httpd = BaseHTTPServer.HTTPServer(('hacker_host', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
-httpd.serve_forever()
